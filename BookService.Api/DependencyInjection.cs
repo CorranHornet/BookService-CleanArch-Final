5926a9f6e5d39a5d940e5af516b3e2adf96a2921
CorranHornet <texhex14@outlook.com>
2026-06-16 10:33:17 +0200

    Add global exception handling and API request pipeline configuration

---
+++ BookService.Api/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace BookService.Api;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApi(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        // Swagger documentation setup
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
---
+++ BookService.Api/Filters/ApiExceptionFilterAttribute.cs
@@ -0,0 +1,27 @@
+﻿using BookService.Application.Common.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BookService.Api.Filters;
+
+public class ApiExceptionFilterAttribute : ExceptionFilterAttribute
+{
+    public override void OnException(ExceptionContext context)
+    {
+        // Handle NotFound exceptions specifically
+        if (context.Exception is NotFoundException notFoundException)
+        {
+            context.Result = new NotFoundObjectResult(new { error = notFoundException.Message });
+            context.ExceptionHandled = true;
+        }
+        else
+        {
+            // Handle all other exceptions as 500 Internal Server Error
+            context.Result = new ObjectResult(new { error = "An internal server error occurred." })
+            { StatusCode = 500 };
+            context.ExceptionHandled = true;
+        }
+
+        base.OnException(context);
+    }
+}
