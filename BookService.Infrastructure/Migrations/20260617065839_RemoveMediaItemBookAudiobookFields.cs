422a89513dc04f0f852817860ce70e164c96a708
CorranHornet <texhex14@outlook.com>
2026-06-17 12:01:10 +0200

    chore(infrastructure): update EF migration for MediaUnit model changes

---
+++ BookService.Infrastructure/Migrations/20260617065839_RemoveMediaItemBookAudiobookFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMediaItemBookAudiobookFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurationMinutes",
+                table: "MediaItems");
+
+            migrationBuilder.DropColumn(
+                name: "PageCount",
+                table: "MediaItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DurationMinutes",
+                table: "MediaItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PageCount",
+                table: "MediaItems",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
